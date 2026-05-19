cd73d467f85821e954eb0fb524233b3938690686
ChrisMouahe <chrismouahe1613@gmail.com>
2026-05-19 22:27:08 +0000

    relevant message

---
+++ Week1/Day1/DailyChallenges/Challenges.py
@@ -0,0 +1,45 @@
+# #Challenge 1
+
+# # Ask the user for a number and a length.
+# # Create a program that prints a list of multiples of the number until the list length reaches length.
+# # Examples
+
+  # # number: 7 - length 5 ➞ [7, 14, 21, 28, 35]
+
+  # # number: 12 - length 10 ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+
+  # # number: 17 - length 6 ➞ [17, 34, 51, 68, 85, 102]
+
+nb = int(input('Type a number please : '))
+taille = int(input("Type another number for the length : "))
+liste = []
+i = 0
+while i < taille :
+      liste.append(nb * (i + 1))
+      i += 1
+print('Number : ',nb,' length : ',taille,'\n', liste)
+
+
+# # Challenge 2
+
+# # Write a program that asks a string to the user, and display a new string with any duplicate consecutive letters removed.
+
+# # Examples
+
+      # # user's word : "ppoeemm" ➞ "poem"
+
+      # # user's word : "wiiiinnnnd" ➞ "wind"
+
+      # # user's word : "ttiiitllleeee" ➞ "title"
+
+      # # user's word : "cccccaaarrrbbonnnnn" ➞ "carbon"
+
+chaine = input("Entrez une chaîne : ")
+
+resultat = ""
+
+for i in range(len(chaine)):
+    if i == 0 or chaine[i] != chaine[i - 1]:
+        resultat = resultat + chaine[i]
+
+print("Nouvelle chaîne :", resultat)

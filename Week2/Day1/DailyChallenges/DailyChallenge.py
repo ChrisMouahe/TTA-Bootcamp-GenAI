fa398c335d1278cc1d41927d19c4e73f250c7ad5
ChrisMouahe <chrismouahe1613@gmail.com>
2026-05-26 08:02:15 +0000

    relevant message

---
+++ Week2/Day1/DailyChallenges/DailyChallenge.py
@@ -0,0 +1,16 @@
+
+"""
+En 2020, quelques mois après l'apparition de la nouvelle épidémie mondiale, la COVID-19, elle ne cessait de gagner du terrain de façon exponentielle. 
+N'ayant pas pu contenir le virus, les gouvernements se sont vus obligés de prendre des décisions rapides pour la protection des populations afin de pouvoir mieux contrôler son évolution. 
+
+En vue de mieux cerner l'ampleur de la situation, ils ont dû analyser des données précises telles que le taux de contamination, la mobilité des populations (Google Mobility Data), la capacité hospitalière, l'âge des patients... 
+En raison du caractère urgent et médical de la situation, ils ont opté pour une analyse prédictive (SIR Models). 
+Le machine learning et la visualisation de données en temps réel qui ont accompagné cette méthode ont permis d'affiner les prédictions et d'orienter les décisions gouvernementales.
+
+Concrètement, cette approche analytique des données a été le meilleur moyen pour les gouvernements d'avoir une estimation des futurs décès supplémentaires qui se chiffrait en milliers de gens. 
+En se basant sur les prédictions des analyses de ces différentes données, selon le rapport de l'Impérial College London (Mars 2020), le Royaume-Uni a décidé son premier confinement. 
+
+N'eût été cette analyse, les prédictions auraient été abstraites et pures suppositions, empêchant ainsi une réaction efficace des gouvernements face à la pandémie.
+Cela aurait inévitablement entrainé des milliers de morts supplémentaires et un effondrement des systèmes mondiaux.
+
+"""
